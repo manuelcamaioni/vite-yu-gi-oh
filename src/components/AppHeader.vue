f649f633b0969d6259a2b6b02f9e899fbb924656
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-19 17:06:05 +0200

    deleted style.css and implemented AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>header</h1>
+    <header class="d-flex align-items-center">
+        <img
+            src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+            alt=""
+            class="w-25" />
+        <h1>Yu-Gi-Oh API</h1>
+    </header>
 </template>
 
 <script>
