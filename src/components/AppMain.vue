6257288fd72cac68f3aba57bb3ae3ff5306adfbd
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-20 12:46:18 +0200

    added select component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <template>
     <main>
+        <SearchByArchetype :ArchetypeList="ArchetypeList" />
         <CardsContainer :CardsList="CardsList" />
     </main>
 </template>
@@ -7,31 +8,63 @@
 <script>
 import CardsContainer from "./CardComponents/CardsContainer.vue";
 import axios from "axios";
+import SearchByArchetype from "./SearchByArchetype.vue";
 export default {
     data() {
         return {
             CardsList: [],
-            apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+            ArchetypeList: [],
+            apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=78",
+            searched: this.searchCards(),
         };
+    },
+    methods: {
+        searchCards() {
+            axios
+                .get(this.apiURL)
+                .then((response) => {
+                    // handle success
+
+                    this.CardsList = response.data.data;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        },
+        getArchetypes() {
+            axios
+                .get(`${this.apiURL}`)
+                .then((response) => {
+                    // handle success
+                    console.log(response.data.data);
+                    response.data.data.forEach((element, index) => {
+                        if (
+                            element.archetype !== undefined &&
+                            !this.ArchetypeList.includes(element.archetype)
+                        ) {
+                            this.ArchetypeList.push(element.archetype);
+                        }
+                    });
+
+                    console.log(this.ArchetypeList);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        },
     },
     components: {
         CardsContainer,
+        SearchByArchetype,
     },
 
     created() {
-        axios
-            .get(this.apiURL)
-            .then((response) => {
-                // handle success
-
-                setTimeout(() => {
-                    this.CardsList = response.data.data;
-                }, 5000);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            });
+        setTimeout(() => {
+            this.searchCards();
+            this.getArchetypes();
+        }, 3000);
     },
 };
 </script>
---
+++ src/components/SearchByArchetype.vue
@@ -0,0 +1,26 @@
+<template>
+    <select class="form-select" aria-label="Default select example">
+        <option>Open this select menu</option>
+        <option v-for="(archetype, index) in ArchetypeList" :value="index + 1">
+            {{ archetype }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    props: {
+        ArchetypeList: Array,
+    },
+    mounted() {
+        // setTimeout(() => {
+        //     console.log(this.CardsList, "originale");
+        // }, 5000);
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
